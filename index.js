6111728d7077bc83964ca98e86daf5ae426effa8
kurosawanao <kurosawanao@team-lab.com>
2024-04-30 12:05:41 +0900

    fix: 反映されない問題

--- index.js
+++ index.js
@@ -3,15 +3,17 @@
 (() => {
   const textarea = document.querySelector("textarea");
   const button = document.querySelector("button");
+  const pre = document.querySelector("pre");
 
-  if (!textarea || !button) return;
-
-  const text = textarea.value;
+  if (!textarea || !button || !pre) return;
 
   button.addEventListener("click", () => {
+    const text = textarea.value;
     navigator.clipboard.writeText(text).then(
-      () => {
+      async () => {
         console.log("Copied to clipboard");
+        const readText = await navigator.clipboard.readText();
+        pre.textContent += `copied text: ${readText}\n`;
       },
       (err) => {
         alert("Failed to copy to clipboard");
