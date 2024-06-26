954c86bec140ded82c8581ff98c741910ea9cad4
Nao KUROSAWA <111628485+rascalNoShippo@users.noreply.github.com>
2024-06-26 15:42:13 +0900

    lineシェアボタン

--- index.js
+++ index.js
@@ -27,6 +27,6 @@
 
   const line = document.querySelector("#line");
   line?.addEventListener("click", () => {
-    open(`https://social-plugins.line.me/lineit/share?url=${encodeURIComponent("https://www.google.com/")}&text=test`);
+    location.href = `https://social-plugins.line.me/lineit/share?url=${encodeURIComponent("https://www.google.com/")}&text=test`
   })
 })();
